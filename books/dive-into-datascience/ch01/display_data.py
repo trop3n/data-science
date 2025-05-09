d1890f504da55f64b8e0412944a3e93392eba1ae
trop3n <jasondkimm@proton.me>
2025-05-08 21:26:17 -0500

    added other statistics functions as an example

--- books/dive-into-datascience/ch01/display_data.py
+++ books/dive-into-datascience/ch01/display_data.py
@@ -1,3 +1,8 @@
 import pandas as pd 
 hour=pd.read_csv('hour.csv')
 print(hour.head())
+print(hour['count'].mean())
+print(hour['count'].median())
+print(hour['count'].std())
+print(hour['registered'].min())
+print(hour['registered'].max())
