1bf9dd5190fd91a978f960c214fc479ba8c98b99
trop3n <jasondkimm@proton.me>
2025-05-08 22:09:44 -0500

    checking for ridership conts based on early morning temps

--- books/dive-into-datascience/ch01/nighttime_data.py
+++ books/dive-into-datascience/ch01/nighttime_data.py
@@ -4,3 +4,8 @@ hour=pd.read_csv('hour.csv')
 print(hour.loc[3, 'count'])
 print(hour.loc[2:4, 'registered'])
 print(hour.loc[hour['hr']<5, 'registered'].mean())
+
+# check for ridership counts on colder early mornings or warmer early mornings:
+
+print(hour.loc[(hour['hr']<5) & (hour['temp']<.50), 'count'].mean())
+print(hour.loc[(hour['hr']<5) & (hour['temp']>.50), 'count'].mean())
